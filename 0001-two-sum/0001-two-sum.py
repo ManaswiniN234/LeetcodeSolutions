681905fb2a4c0563350cb556a9e2426ca02078a2
Manaswini N <manaswini.n234@gmail.com>
2026-07-02 09:40:35 +0530

    Time: 1 ms (62.58%), Space: 12.9 MB (81.83%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dict1 = {}
+        for i , num in enumerate(nums):
+            x = target - num
+            if x in dict1:
+                return[dict1[x], i]
+            dict1[num] = i
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
